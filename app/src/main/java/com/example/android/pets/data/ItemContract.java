b914b7a07cec352a3c3d2f027f2cf81860cac0cb
Tony <thanhlong.le2002@gmail.com>
2017-03-11 12:20:10 +0700

    this is my app

---
+++ app/src/main/java/com/example/android/pets/data/ItemContract.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.pets.data;
+
+import android.net.Uri;
+import android.content.ContentResolver;
+import android.provider.BaseColumns;
+
+/**
+ * API Contract for the Pets app.
+ */
+public final class ItemContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private ItemContract() {}
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_ITEMS = "items";
+
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class itemEntry implements BaseColumns {
+
+        /** The content URI to access the item data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_ITEMS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of items.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ITEMS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single item.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_ITEMS;
+
+        /** Name of database table for pets */
+        public final static String TABLE_NAME = "items";
+
+        /**
+         * Unique ID number for the item (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the item.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_ITEM_NAME ="name";
+
+        /**
+         * Supplier of the item.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_ITEM_SUPPLIER = "supplier";
+
+        /**
+         * Status of the pet.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_ITEM_STATUS = "status";
+
+        /**
+         * Weight of the item.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_ITEM_WEIGHT = "weight";
+
+        /**
+         * Possible values for the status of the item.
+         */
+        public static final int STATUS_GOOD = 0;
+        public static final int STATUS_DELIVERING = 1;
+        public static final int STATUS_QUEUE = 2;
+
+        /**
+         * Returns whether or not the given gender is {@link #STATUS_GOOD}, {@link #STATUS_DELIVERING},
+         * or {@link #STATUS_QUEUE}.
+         */
+        public static boolean isValidStatus(int gender) {
+            if (gender == STATUS_GOOD || gender == STATUS_DELIVERING || gender == STATUS_QUEUE) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
+
